f8a7bb67b40e264c346e56320d121702cc652ff9
Rianna Cantarelli <rianna.aeon@gmail.com>
2020-04-05 00:51:19 -0300

    Reorder all

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -1,7 +1,9 @@
 <?php
 $rootDir = realpath(__DIR__ . "/..");
 require_once $rootDir . "/vendor/autoload.php";
-require_once $rootDir . "/vendor/aeondigital/phpstream/tests/src/__provider.php";
+
+
+/*require_once $rootDir . "/vendor/aeondigital/phpstream/tests/src/__provider.php";
 require_once $rootDir . "/vendor/aeondigital/phphttpuri/tests/src/__provider.php";
 require_once $rootDir . "/vendor/aeondigital/phphttpdata/tests/src/__provider.php";
 require_once $rootDir . "/vendor/aeondigital/phphttpmessage/tests/src/__provider.php";
@@ -12,3 +14,4 @@ require_once $rootDir . "/tests/src/concrete/Middleware01.php";
 require_once $rootDir . "/tests/src/apps/site/AppStart.php";
 require_once $rootDir . "/tests/src/apps/site/controllers/Home.php";
 require_once $rootDir . "/tests/src/apps/site/middlewares/TestMiddleware.php";
+*/
